add97405646beac37add4d81b92bb6a266de5168
hieunn <hieunn@vicoders.com>
2021-07-19 08:37:09 +0700

    chore: remove unused file

--- tests/Stubs/Models/Comment.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace VCComponent\Laravel\Comment\Test\Stubs\Models;
-
-use VCComponent\Laravel\Comment\Entities\Comment as EntitiesComment;
-
-class Comment extends EntitiesComment
-{
-}
